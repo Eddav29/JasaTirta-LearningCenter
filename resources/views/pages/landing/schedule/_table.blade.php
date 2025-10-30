1df17a04618bbcaf7fe4b880632b1cbafee5e869
ikhwandio4 <basroni12062018@gmail.com>
2025-10-30 14:12:46 +0700

    feat: Enhance hero section with dynamic background, animations, and updated content; add schedule table ID for navigation

--- resources/views/pages/landing/schedule/_table.blade.php
+++ resources/views/pages/landing/schedule/_table.blade.php
@@ -1,5 +1,5 @@
 {{-- Schedule Table Section --}}
-<section class="pb-12">
+<section id="schedule-table" class="pb-12">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
         @php
         $scheduleData = [
