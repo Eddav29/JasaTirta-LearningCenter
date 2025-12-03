2594abf2aed837f01ac6ffd463a668e100e752b5
Eddo Dava <eddodava@gmail.com>
2025-12-03 14:37:27 +0700

    fix(app): update application name and title for consistency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} - @yield('title', 'Learning Center')</title>
+    <title>JTLC - Jasa Tirta Learning Center</title>
+
+    {{-- logo --}}
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
